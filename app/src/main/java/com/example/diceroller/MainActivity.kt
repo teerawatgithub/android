cf79a96129f9907cf911a0b8e0ffdd6e78dd5987
teerawatgithub <59160069@go.buu.ac.th>
2019-08-11 18:03:14 +0700

    reset button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
 
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{ resetDice() }
+
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = "Dice Rolled"
     }
@@ -25,5 +28,10 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun resetDice(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
     }
 }
