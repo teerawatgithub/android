513e2227888695c8e8ac93e351bd7b9d9ab72186
teerawatgithub <59160069@go.buu.ac.th>
2019-08-11 17:38:54 +0700

    Task: Change the text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +16,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
 
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled"
     }
 
     private fun rollDice(){
+        val randomInt = Random().nextInt(6)+1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
